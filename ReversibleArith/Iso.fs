57da4024fa465b4857cec0898dad279b9a8ff8f7
Ming <project.shinkirou@gmail.com>
2021-01-23 10:16:47 -0800

    Add ReversibleArith

---
+++ ReversibleArith/Iso.fs
@@ -0,0 +1,171 @@
+﻿module Iso
+
+module Widths =
+  open System
+  open System.Collections.Generic
+  let widths : Dictionary<Type, int> = Dictionary()
+  let add t i = widths.[t] <- i
+  let rec get (t : Type) = 
+    if widths.ContainsKey(t) then
+      Some widths.[t]
+    elif t.IsGenericType && t.GetGenericTypeDefinition() = typedefof<unit * unit> then
+        match t.GetGenericArguments() with
+        | [| a; b |] -> Option.map2 (+) (get a) (get b)
+        | _ -> None
+    else
+      None
+
+  let addToWidths t x = widths.[t] <- x
+
+  let inline getFrom<'t>() = 
+    let t = typeof<'t>
+    let res = get t 
+    if not <| widths.ContainsKey(t) then
+      if Option.isNone res then
+        eprintfn "failed: %O" t
+      res |> Option.iter (addToWidths t)
+    res
+
+type SymIsoType =
+| TNum of int list
+| TDigit of int
+| TPair of SymIsoType * SymIsoType
+
+type SymIsoFunc =
+| FId of int option
+| FNum of _bases : int list
+| FSucc of _base : int
+| FComm of (int * int) option
+| FAssoc of (int * int * int) option
+
+type SymIsoPFunc =
+| PFRep of _base : int
+| PFCond of value : int * _base : int
+| PFCondLast of _base : int
+
+let funcNameLaTeX =
+  function
+  | FId None -> sprintf "I"
+  | FId (Some n) -> sprintf "I_{%d}" n
+  | FNum [b] ->
+    sprintf "N_{%d}" b
+  | FNum (b :: bs) ->
+    Seq.map string bs
+    |> String.concat ", "
+    |> sprintf "N_{%d,(%s)}" b
+  | FNum bs ->
+    Seq.map string bs
+    |> String.concat ", "
+    |> sprintf "N_{%s}"
+  | FSucc b -> sprintf "S_{%d}" b
+  | FComm None -> "C"
+  | FComm (Some(a, b)) -> sprintf "C_{%d,%d}" a b
+  | FAssoc None -> "A"
+  | FAssoc (Some(a, b, c)) -> sprintf "A_{(%d,%d),%d}" a b c
+
+let pFuncNameLaTeX =
+  function
+  | PFRep b -> sprintf "R_{%d}" b
+  | PFCond (i, b) -> sprintf "C_{%d,%d}" i b
+  | PFCondLast b -> sprintf "CL_{%d}" b
+
+type SymIso =
+| SGroup of string * SymIso
+| SFunc of SymIsoFunc
+| SPFunc of SymIsoPFunc * SymIso list
+| SSym of SymIso
+| SPair of SymIso * SymIso
+| SCompose of SymIso list
+
+let rec symIsoToObj si : obj =
+  let compose (fs : obj) (gs : obj) : obj list =
+    match fs, gs with
+    | (:? list<obj> as fs'), (:? list<obj> as gs') -> fs' @ gs'
+    | (:? list<obj> as fs'), _ -> fs' @ [gs]
+    | _, (:? list<obj> as gs') -> [fs] @ gs'
+    | _ -> [fs; gs]
+  match si with
+  | SGroup(_, g) -> symIsoToObj g
+  | SSym s -> box ("$sym", symIsoToObj s)
+  | SFunc f -> box f
+  | SPFunc(s, xs) -> box (s, List.map symIsoToObj xs)
+  | SPair(a, b) -> box (symIsoToObj a, symIsoToObj b)
+  | SCompose [] -> box "id"
+  | SCompose [x] -> symIsoToObj x
+  | SCompose xs -> 
+    xs 
+    |> List.map symIsoToObj 
+    |> List.reduce (fun a b -> box (compose a b)) 
+    |> box
+
+let rec symIsoToLaTeX depth si : string =
+  let rec flattenCompose xs =
+    let expandCompose = function SCompose xs -> xs | x -> [x]
+    xs 
+    |> List.map expandCompose
+    |> List.concat
+
+  let up s = sprintf "\\mathrm{%s}" s
+  let op s = sprintf "\\operatorname{%s}" s
+  let recurse = symIsoToLaTeX depth
+  match si with
+  | SGroup(n, g) -> 
+    match depth with
+    | None -> symIsoToLaTeX None g
+    | Some 0 -> " \\ldots "
+    | Some i -> symIsoToLaTeX (Some (i - 1)) g
+    |> sprintf " \\stackrel{\\rm %s}{\\boxed{%s}} " n
+  | SSym(SFunc s) -> sprintf "!%s" (up <| funcNameLaTeX s)
+  | SSym s -> sprintf "!\\left[ %s \\right]" (recurse s)
+  | SFunc f -> up (funcNameLaTeX f)
+  | SPFunc(s, xs) -> 
+    sprintf "\\stackrel{\\large\\langle %s \\rangle}{\\boxed{%s}}" 
+      (op <| pFuncNameLaTeX s) 
+      (List.map recurse xs |> String.concat " , ")
+  | SPair(a, b) -> sprintf "\\begin{pmatrix} %s \\\\ %s \\end{pmatrix}" (recurse a) (recurse b)
+  | SCompose [] -> "I"
+  | SCompose [x] -> recurse x
+  | SCompose xs -> 
+    xs 
+    |> flattenCompose
+    |> List.map recurse
+    |> String.concat " \\cdot "
+    |> sprintf " %s "
+
+type Iso<'a, 'b> = Iso of f : ('a -> 'b) * g : ('b -> 'a) * si : SymIso
+
+type BIso<'a, 'b> = unit -> Iso<'a, 'b>
+let inline BIso(f, g, s) : BIso<_, _> = fun () -> Iso(f, g, s)
+let inline (|BIso|) (x : BIso<_, _>) = match x() with Iso(f, g, s) -> f, g, s
+
+let group n (BIso(a, b, s)) = BIso(a, b, SGroup(n, s))
+
+// let showBIso (BIso(_, _, s)) = (sprintf "%A" <| symIsoToObj s).Replace("\"", "")
+let showBIso n (BIso(_, _, s)) = sprintf "%s" <| symIsoToLaTeX n s
+let showBIso' (BIso(_, _, s)) = sprintf "%A" s
+
+let (<<|) (BIso(f, _, _)) x = f x
+let (>>>) (BIso(f, f', fs)) (BIso(g, g', gs)) = 
+  BIso(f >> g, g' >> f', SCompose [fs; gs])
+
+let (&&&) (BIso(f, f', fs)) (BIso(g, g', gs)) =
+  BIso((fun (x, y) -> (f x, g y)), (fun (x, y) -> (f' x, g' y)), SPair(fs, gs))
+
+let sym (BIso(a, b, s)) =
+  BIso(b, a, SSym s)
+
+let inline (~~) x = sym x
+
+let id() : Iso<'a, 'a> = 
+  Iso((fun x -> x), (fun x -> x), SFunc(FId <| Widths.getFrom<'a>()))
+
+let comm() : Iso<'a * 'b, 'b * 'a> = 
+  let wa, wb = Widths.getFrom<'a>(), Widths.getFrom<'b>()
+  Iso((fun (x, y) -> y, x), (fun (x, y) -> y, x), 
+      SFunc(FComm <| Option.map2 (fun a b -> a, b) wa wb))
+
+let assoc() : Iso<('a * 'b) * 'c, _> = 
+  let wa, wb, wc = Widths.getFrom<'a>(), Widths.getFrom<'b>(), Widths.getFrom<'c>()
+  Iso((fun ((x, y), z) -> x, (y, z)), (fun (x, (y, z)) -> (x, y), z), 
+      SFunc (FAssoc <| Option.map3 (fun a b c -> a, b, c) wa wb wc))
+
---
+++ ReversibleArith/ReversibleArith.fs
@@ -0,0 +1,172 @@
+﻿module ReversibleArith
+open Iso
+
+type IBases =
+  abstract member Bases : int list
+
+let inline getBases (x : #IBases) = x.Bases
+let inline (|Bases|) (x : #IBases) = getBases x
+
+type INum =
+  abstract member NumberValue : int
+  abstract member Base : int
+  inherit IBases
+
+let inline numberValue (x : #INum) = x.NumberValue
+let inline baseValue (x : #INum) = x.Base
+
+type IBase =
+  abstract member Base : int
+
+let mkBase x = { new IBase with member b.Base = x }
+
+type B2 = B2 with
+  interface IBase with member b.Base = 2
+
+type B10 = B10 with
+  interface IBase with member b.Base = 10
+
+let inline getBase (x : #IBase) =
+  (x :> IBase).Base
+
+let inline (|Base|) x = getBase x
+
+type Digit<'b when 'b :> IBase> = Digit of digit : int * _base : 'b with
+  interface INum with
+    member d.NumberValue =
+      match d with Digit(d, _) -> d
+
+    member d.Base =
+      match d with Digit(_, b) -> getBase b
+
+    member d.Bases =
+      match d with Digit(d, _) -> [d]
+
+let succDigit (Digit(d, (Base b as b'))) = Digit((d + 1) % b, b')
+let predDigit (Digit(d, (Base b as b'))) = Digit((d + b - 1) % b, b')
+
+type Num<'b, 'n when 'b :> IBase and 'n :> INum> = Num of Digit<'b> * 'n with
+  interface INum with
+    member n.Bases =
+      match n with
+      | Num(d, Bases ns) -> [(d :> INum).Base] @ ns
+
+    member n.NumberValue =
+      match n with 
+      | Num(x, y) -> (numberValue x) + (x :> INum).Base * (numberValue y)
+
+    member n.Base =
+      match n with Num(x, y) -> (x :> INum).Base * y.Base
+
+let succ b = BIso(succDigit, predDigit, SFunc (FSucc (b :> IBase).Base))
+let unNum (Num(a, b)) = a, b
+let mkNum (a, b) = Num(a, b)
+
+let num (Bases bs) = BIso(mkNum, unNum, SFunc (FNum bs))
+
+let cond (Digit(a, b) as i) (BIso(f, g, s)) =
+  BIso((fun (v, x) -> (v, (if i = v then f x else x))),
+       (fun (v, x) -> (v, (if i = v then g x else x))), SPFunc(PFCond(a, b.Base), [s]))
+
+let condLast (b0 : 'b when 'b :> IBase) (BIso(f, g, s)) =
+  BIso((fun ((Digit(n, (Base b) : 'b) as v), x) -> (v, (if n + 1 = b then f x else x))),
+       (fun ((Digit(n, Base b) as v), x) -> (v, (if n + 1 = b then g x else x))),
+       SPFunc(PFCondLast b0.Base, [s]))
+
+let rep (Base b) (BIso(f, g, s)) =
+  let rec rep i f x =
+    if i <= 0 then x else rep (i - 1) f (f x)
+  BIso((fun ((v : Digit<_>), x) -> (v, rep (numberValue v) f x)),
+       (fun (v, x) -> (v, rep (numberValue v) g x)),
+       SPFunc(PFRep b, [s]))
+
+let rep' b bi = (comm >>> rep b bi >>> comm)
+
+[<AutoOpen>]
+module Builders =
+  type SuccBuilder<'b when 'b :> INum> =
+    abstract member Succ : BIso<'b, 'b>
+
+  type AddBuilder<'b when 'b :> INum> =
+    abstract member Add : BIso<'b * 'b, 'b * 'b>
+
+  type SuccAddBuilder<'b when 'b :> INum> =
+    inherit SuccBuilder<'b>
+    inherit AddBuilder<'b>
+    inherit IBases
+    
+  let private basesName (x : #SuccAddBuilder<_>) =
+    x.Bases
+    |> Seq.map string
+    |> String.concat ","
+
+  type SuccDigit<'b when 'b :> IBase> = private SuccDigit of 'b with
+    interface SuccAddBuilder<Digit<'b>> with
+      member d.Succ = match d with SuccDigit b -> succ b
+      member d.Add = match d with SuccDigit b -> rep' b (succ b)
+      member d.Bases = match d with SuccDigit b -> [b.Base] 
+    
+  type SuccNum<'b, 'n when 'b :> IBase and 'n :> INum> = private SuccNum of 'b * SuccAddBuilder<'n> with
+    interface SuccAddBuilder<Num<'b, 'n>> with
+      member s.Succ = 
+        let (SuccNum (b, s')) = s
+        let num = num (s :> SuccAddBuilder<_>)
+        sym num >>> condLast b s'.Succ >>> (succ b &&& id) >>> num
+        |> group (sprintf "succ(%s)" <| basesName s)
+
+      member s.Add = 
+        let (SuccNum (b, s')) = s
+        let num, num' = num (s :> SuccAddBuilder<_>), num (s' :> SuccAddBuilder<_>)
+        let succ = (s :> SuccAddBuilder<_>).Succ
+        let splitDigits = assoc >>> (id &&& (sym assoc >>> (comm &&& id) >>> assoc)) >>> sym assoc
+        let joinA = (id &&& (sym assoc >>> (num' &&& id))) >>> sym assoc
+        let addB = ((rep b succ >>> comm) &&& id)
+        let join = assoc >>> joinA >>> addB >>> assoc
+        ((sym num &&& sym num) >>> splitDigits >>> (comm &&& s'.Add) >>> join >>> (id &&& num))
+        |> group (sprintf "add(%s)" <| basesName s)
+
+      member s.Bases = 
+        let (SuccNum (b, s')) = s
+        [b.Base] @ s'.Bases
+
+  let succDigit (b : 't when 't :> IBase) = 
+    let sd = SuccDigit b
+    Widths.add typeof<Digit<'t>> (getBase b)
+    sd
+
+  let succNum (b : 'b when 'b :> IBase) (n : SuccAddBuilder<'n>) = 
+    let sn = SuccNum(b, n)
+    Widths.add typeof<Digit<'b>> (getBase b)
+    Widths.add (typeof<Num<'b, 'n>>) (getBases sn |> List.sum)
+    sn
+
+let add b = rep' b (succ b) |> group (sprintf "add(%d)" b.Base)
+let mult b = rep' b (add b) |> group (sprintf "mult(%d)" b.Base)
+
+[<EntryPoint>]
+let main argv =
+  let s = succNum B10 (succNum B10 (succNum B10 (succDigit B10)))
+  let sn = (s :> SuccBuilder<_>).Succ
+  let addn = (s :> AddBuilder<_>).Add
+  printfn "%A" <| numberValue (Num(Digit(2, B10), Num(Digit(3, B10), Digit(4, B10))))
+  printfn "%A" <| (add B10 <<| (Digit(7, B10), Digit(6, B10)))
+  printfn "%A" <| (mult B10 <<| ((Digit(2, B10), Digit(3, B10)), Digit(4, B10)))
+
+  let d5499 = Num(Digit(9, B10), Num(Digit(9, B10), Num(Digit(4, B10), Digit(5, B10))))
+  let d1237 = Num(Digit(7, B10), Num(Digit(3, B10), Num(Digit(2, B10), Digit(1, B10))))
+  printfn "%A" <| (sn <<| d5499)
+  printfn "%A" <| (addn <<| (d5499, d1237))
+  printfn "%A" <| (addn <<| (d1237, d5499))
+
+  printfn ""
+  printfn ""
+  printfn "%s" (showBIso None sn)
+  printfn "\\\\"
+  printfn "%s" (showBIso None addn)
+
+  // printfn ""
+  // printfn ""
+  // printfn "%s" (showBIso' sn)
+  // printfn "%s" (showBIso' addn)
+  0 // return an integer exit code
+
